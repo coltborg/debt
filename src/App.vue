1ccdbb3dd037601650805a0cbddf5d4ce3d887c7
coltborg <coltborg@gmail.com>
2020-01-05 16:11:57 -0600

    Add composition api

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,17 @@
 <template>
   <div id="app">
-    <h1 class="text-3xl text-green-400">Hello world</h1>
+    <h1 class="text-3xl text-green-400">{{ message }}</h1>
   </div>
 </template>
 
 <script>
+import { ref } from "@vue/composition-api";
+
 export default {
-  name: "app"
+  name: "app",
+  setup() {
+    const message = ref("🔥 Yo Vue 3 🔥");
+    return { message };
+  }
 };
 </script>
